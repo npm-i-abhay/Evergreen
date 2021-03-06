d083e28c5428c3aca945873deb64ec30822ad84b
marshall-666 <abakshi4@my.bcit.ca>
2021-03-05 23:53:44 -0800

    made animating flies using png image

---
+++ comps/flies/index.js
@@ -0,0 +1,19 @@
+function moveFlies(animName="flyfly")
+{
+    return`
+    <div style="
+    position: relative; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    width: 100px; 
+    height: 100px;
+    border: 2px solid black; " id="fly">
+        
+        
+        <img  style=" position: absolute; animation-name: ${animName}; animation-duration: 3s;" src="./images/Fly.png" alt="">
+    </div>
+
+    `
+}
--- workspace.js
+++ workspace.js
@@ -1,3 +1,5 @@
 document.querySelector("#item1").innerHTML += animateTruck();
 document.querySelector("#item2").innerHTML += handleRecycle();
 document.querySelector("#globemawartz").innerHTML += handleEarth()
+document.querySelector("#flies").innerHTML += moveFlies()
+document.querySelector("#flies").innerHTML += moveFlies("flyflytwo")
