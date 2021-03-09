cbaeb9b655dc0a46d34a192ee77d3ae135963d8f
nickbarr3160 <nbarr2@my.bcit.ca>
2021-03-09 13:29:52 -0800

    Adding Abhay's components

---
+++ comps/Scroll/index.js
@@ -0,0 +1,12 @@
+function scrollUI(){
+    return`
+    <img src="/images/scroll.png" id="scrollIcon" onclick='scrollUI.scrollAnimation()'>
+
+    `
+}
+
+
+
+//eddyUI.chatBubble = () =>{
+ //   document.getElementById("chat2").style.opacity="100";
+//}
--- workflow.js
+++ workflow.js
@@ -3,3 +3,4 @@ document.querySelector(".main").innerHTML += TravisUI();
 document.querySelector(".main").innerHTML += infoUI();
 document.querySelector(".main").innerHTML += chatUI();
 document.querySelector(".main").innerHTML += TrashUI();
+document.querySelector(".main").innerHTML += scrollUI();
