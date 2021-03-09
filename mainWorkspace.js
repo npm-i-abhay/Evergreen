630f9b410c5b65a45fcc8cfb6079c504916c5a04
marshall-666 <abakshi4@my.bcit.ca>
2021-03-09 15:59:22 -0800

    adding some more images and animated objects

--- mainWorkspace.js
+++ mainWorkspace.js
@@ -2,6 +2,6 @@ document.querySelector("#globe").innerHTML += handleEarth();
 document.querySelector("#flies").innerHTML += moveFlies()
 document.querySelector("#flies").innerHTML += moveFlies("flyflytwo")
 
-document.querySelector("#dumpTruck").innerHTML = animateTruck();
+document.querySelector("#dumpTruck").innerHTML += animateTruck();
 
 document.querySelector("#green").innerHTML += handleRecycle ();
