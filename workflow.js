4343beb53983df82a1f5dba2fcf0ccae8affd80d
nickbarr3160 <nbarr2@my.bcit.ca>
2021-03-10 13:58:30 -0800

    Merge branch 'abhay' of https://github.com/marshall-666/Evergreen into abhay

--- workflow.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".main").innerHTML += eddyUI();
-document.querySelector(".main").innerHTML += TravisUI();
-document.querySelector(".main").innerHTML += infoUI();
-document.querySelector(".main").innerHTML += chatUI();
-document.querySelector(".main").innerHTML += TrashUI();
-document.querySelector(".main").innerHTML += scrollUI();
