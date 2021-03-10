3e0b75efa80a2cd46c8e0fc78aadf04a1014bf19
marshall-666 <abakshi4@my.bcit.ca>
2021-03-09 17:05:42 -0800

    minimal changes

--- mainWorkspace.js
+++ mainWorkspace.js
@@ -5,8 +5,11 @@ document.querySelector("#flies").innerHTML += moveFlies("flyflytwo")
 
 document.querySelector("#dumpTruck").innerHTML += animateTruck();
 document.querySelector("#garbageItem1").innerHTML += handleGarbage();
-document.querySelector("#garbageItem2").innerHTML += handleGarbage("0em", "25em", "./images/garbage/appple 1.png");
+
+document.querySelector("#garbageItem2").innerHTML += handleGarbage("0em", "25em", "./images/garbage/appple 1.png", "");
+
 document.querySelector("#garbageItem3").innerHTML += handleGarbage("0em", "35em", "./images/garbage/Bananna Peel.png");
-document.querySelector("#garbageItem4").innerHTML += handleGarbage("0em", "5em", "./images/garbage/garbage bag.png");
+
+document.querySelector("#garbageItem4").innerHTML += handleGarbage("0em", "5em", "./images/garbage/garbage bag.png", "fallingtwo");
 
 document.querySelector("#green").innerHTML += handleRecycle ();
